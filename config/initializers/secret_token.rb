3c7d53abfdd0817fe526a58a965564053948262d
Jarek <tuc@ubuntu.(none)>
2013-04-22 11:46:09 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsLists::Application.config.secret_token = 'd0655737ad693f160fdac25e4372b0d4b16512adba60e97b799ba811e787775f9b980731ee1f2fbe80648fc4bd081ec133b15ad8dfb069f074408cc2d0e8764d'
